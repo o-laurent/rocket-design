fd72f2039c618604658e39c9025cec69ec4f1b2e
M. Johnny Yammine <johnny.yammine@ensta-paris.fr>
2020-04-03 12:30:40 +0200

    Trajectory Class1

--- trajectory.py
+++ trajectory.py
@@ -6,7 +6,7 @@ class Trajectory:
     "Most of the cases it's an elliptic trajectory if it's a cercle we have semi-minor axis = semi-major axis"
     def __init__(self, data):
         self.name = data['Name']
-        self.a = data['semi-major axis']  "Most of the cases it's an elliptic trajectory if it's"
+        self.a = data['semi-major axis']  
         self.b = data['semi-minor axis']
         self.centerx = data['X-center coordinate']
         self.centery = data['Y-center coordinate']
