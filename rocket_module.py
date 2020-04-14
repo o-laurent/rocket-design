52f5d3b6bba0b3d40c2d547f44841daa4ff03e28
Olivier Laurent <olivier.ar.laurent@gmail.com>
2020-04-14 12:09:05 +0200

    added TDD + changed names

--- rocket_module.py
+++ rocket_module.py
@@ -34,3 +34,21 @@ class Rocket:
 
     def getS2Geometry(self):
         return [self.S2length, self.S2diameter] 
+
+class Trajectory:
+    """ 
+        class which contains every useful info about the trajectory we want to reach (GEO,LEO)
+    """
+
+    "Most of the cases it's an elliptic trajectory if it's a cercle we have semi-minor axis = semi-major axis"
+    def __init__(self, data):
+        self.name = data['Name']
+        self.a = data['semi-major axis']  
+        self.b = data['semi-minor axis']
+        self.centerx = data['X-center coordinate']
+        self.centery = data['Y-center coordinate']
+        self.centerz = data['Z-center coordinate']
+        
+
+    def getName(self):
+        return self.name
