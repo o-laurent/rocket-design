740de60c9bbd33d159a8d0f3c115f5e77ff21150
Olivier Laurent <olivier.ar.laurent@gmail.com>
2020-04-18 13:18:03 +0200

    added index

--- gui.py
+++ gui.py
@@ -4,8 +4,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return render_template("index.html", message = "Hello World!")
+@app.route("/index")
+
+def start():
+    return render_template("index.html")
+
+@app.route('/addrocket.html')
+@app.route('/addrocket')
+def addrocket():
+    return render_template("addrocket.html")
 
 if __name__ == "__main__":
     app.run()
