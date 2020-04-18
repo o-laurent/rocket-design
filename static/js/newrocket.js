dd4c60a33a5e9fd48ccde1e2d4663bd200f4d5ae
Olivier Laurent <olivier.ar.laurent@gmail.com>
2020-04-18 12:25:00 +0200

    rocket follow scrolling

---
+++ static/js/newrocket.js
@@ -0,0 +1,26 @@
+/*Need to verify a lot of data*/ 
+document.getElementById("stageNumber").addEventListener("change", function (e){
+    if (document.getElementById('stageNumber').value=='2') {
+        document.getElementById('secondStageDiv').style.display = 'block';
+    }
+    else {
+        document.getElementById('secondStageDiv').style.display = 'none';
+    }
+})
+
+document.getElementById("boosterSelect").addEventListener("change", function (e){
+    if (document.getElementById('boosterSelect').value=='True') {
+        document.getElementById('boosterDiv').style.display = 'block';
+    }
+    else {
+        document.getElementById('boosterDiv').style.display = 'none';
+    }
+})
+
+document.getElementById("rocketName").addEventListener("keyup", function (e){
+    document.getElementById("dName").innerHTML = document.getElementById("rocketName").value
+})
+
+document.getElementById("rocketYear").addEventListener("keyup", function (e){
+    document.getElementById("dYear").innerHTML = document.getElementById("rocketYear").value
+})
