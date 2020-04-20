fcb889bc0dc3400c56bea5f8a8fed7401aa79eb4
M. Johnny Yammine <johnny.yammine@ensta-paris.fr>
2020-04-20 16:35:55 +0200

    Verlet_Rectification

--- physics/verlet.c
+++ physics/verlet.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include "verlet.h"
 
-pointVerlet new_position(x){
-
+pointVerlet* new_position(pointVerlet* u){
+    pointVerlet* v;
+    v->x= 2*u->x - u->old_x + u->ax;
+    v->y= 2*u->y - u->old_y + u->ay;
+    return(v);
 }
